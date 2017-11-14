6b1c294daaef2cc6a18e71df63d27d8f9282d726
Daniel Foster <dfoster8@myseneca.ca>
2017-11-14 13:34:40 -0500

    JLoader class template created

--- app/src/main/java/com/map524/parkit/parkit/SearchActivity.java
+++ app/src/main/java/com/map524/parkit/parkit/SearchActivity.java
@@ -1,13 +1,46 @@
 package com.map524.parkit.parkit;
 
+import android.content.Context;
+import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import com.map524.parkit.parkit.ParkingDataModel;
+
+import java.util.ArrayList;
+
 
 public class SearchActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
+
+        /*JSONLoader nearbyLots = new JSONLoader();
+        ArrayList<ParkingDataModel> parkingLotsGP = nearbyLots.JsonDataModel();
+
+        ListView seaLots = (ListView)findViewById(R.id.listLots);
+
+        NearbyListviewAdapter nblva = new NearbyListviewAdapter(parkingLotsGP, this.getApplicationContext());
+
+        seaLots.setAdapter(nblva);
+
+
+
+        //wire up the listview with the arraylist of parking lots
+        //NearbyListviewAdapter adapter = new NearbyListviewAdapter(parkList, this.getApplicationContext());
+        //seaLots.setAdapter(adapter);
+       setContentView(R.layout.activity_search);
+       */
+
     }
-}
+    }
